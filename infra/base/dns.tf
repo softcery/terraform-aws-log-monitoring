9ff320128746ae849daf466dbcf4d31b6d2e3fec
kibique <danylo.ostrikov@softcery.com>
2023-06-26 15:09:46 +0300

    fix: deleted subdomain creation

--- infra/base/dns.tf
+++ infra/base/dns.tf
@@ -3,16 +3,3 @@ resource "aws_route53_zone" "base" {
   tags = local.tags
 }
 
-resource "aws_route53_record" "subdomain" {
-  zone_id = "Z08391391QN24LNLSINRV"
-  name    = var.domain
-  type    = "NS"
-  ttl     = 172800
-  records = [
-    "${aws_route53_zone.base.name_servers[0]}.",
-    "${aws_route53_zone.base.name_servers[1]}.",
-    "${aws_route53_zone.base.name_servers[2]}.",
-    "${aws_route53_zone.base.name_servers[3]}.",
-  ]
-  allow_overwrite = true
-}
