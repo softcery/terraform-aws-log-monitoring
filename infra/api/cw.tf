4c51f0762ac14cc0d18b4d4a4198a6375db867de
kibique <danylo.ostrikov@softcery.com>
2023-06-28 11:19:10 +0300

    infra: added log dashboard

--- infra/api/cw.tf
+++ infra/api/cw.tf
@@ -5,3 +5,26 @@ resource "aws_cloudwatch_log_group" "api" {
 
   tags = local.tags
 }
+
+resource "aws_cloudwatch_dashboard" "logs" {
+  dashboard_name = "dashboard-${var.name}"
+  dashboard_body = <<EOF
+{
+    "widgets": [
+        {
+            "type": "log",
+            "x": 12,
+            "y": 24,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.region}",
+                "title": "Application logs",
+                "query": "SOURCE '${var.name}' | fields @timestamp, @message, @logStream, @log | sort @timestamp desc| filter @message not like "ping" | limit 100 ",
+                "view": "table"
+            }
+        }
+    ]
+}
+EOF
+}
