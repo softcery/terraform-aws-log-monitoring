bca95639e3685551d181a60c99d44c78d0b316c1
kibique <danylo.ostrikov@softcery.com>
2023-06-29 11:48:26 +0300

    infra: added log table

--- infra/api/cw.tf
+++ infra/api/cw.tf
@@ -7,20 +7,34 @@ resource "aws_cloudwatch_log_group" "api" {
 }
 
 resource "aws_cloudwatch_dashboard" "logs" {
-  dashboard_name = "dashboard-${var.name}"
+  dashboard_name = "dashboard-${var.name}"  
   dashboard_body = <<EOF
 {
-    "widgets": [
+ "widgets": [
         {
-            "type": "log",
-            "x": 0,
-            "y": 0,
+            "height": 5,
             "width": 24,
-            "height": 14,
+            "y": 0,
+            "x": 0,
+            "type": "log",
             "properties": {
                 "region": "${var.region}",
                 "title": "Application logs",
                 "query": "SOURCE '${var.name}' | fields @timestamp, @message, @logStream, @log | sort @timestamp desc | filter @message not like \"ping\" | limit 100",
+                "view": "table"
+            }
+        },
+        {
+            "type": "log",
+            "x": 0,
+            "y": 5,
+            "width": 24,
+            "height": 6,
+            "properties": {
+                "title": "API errors",
+                "query": "SOURCE '${var.name}' | fields @timestamp, @message\n| filter @message like /\"severity\":\"(ERROR|FATAL)\"/\n| sort @timestamp desc",
+                "region": "${var.region}",
+                "stacked": false,
                 "view": "table"
             }
         }
